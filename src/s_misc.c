d2e8f267fb6dca861843cd5862c74c8cad8471b8
Diane Bruce <db@db.net>
1998-07-05 04:18:11 +0000

    Added seconds to /time output, in this day and age of TS servers its useful

--- src/s_misc.c
+++ src/s_misc.c
@@ -101,9 +101,9 @@ char	*date(time_t clock)
   if (minswest < 0)
     minswest = -minswest;
   
-  (void)ircsprintf(buf, "%s %s %d %04d -- %02d:%02d %c%02d:%02d",
+  (void)ircsprintf(buf, "%s %s %d %04d -- %02d:%02d:%02d %c%02d:%02d",
 		   weekdays[lt->tm_wday], months[lt->tm_mon],lt->tm_mday,
-		   lt->tm_year + 1900, lt->tm_hour, lt->tm_min,
+		   lt->tm_year + 1900, lt->tm_hour, lt->tm_min, lt->tm_sec,
 		   plus, minswest/60, minswest%60);
 
   return buf;
